63f28bec94ce6ef3ca09f9cc99c573aacc8bb59e
djarr <raygoza.rem@gmail.com>
2025-04-18 17:56:41 -0600

    Deploy to GitHub Pages

---
+++ dd/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html", // Asegúrate de que esté apuntando a tu archivo HTML
+  "./src/**/*.{js,ts,jsx,tsx}" // Si tienes archivos JS/TS
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
