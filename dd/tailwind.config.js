0c0277008c3c3c6e9b6067666ab9e9bb468bb586
djarr <raygoza.rem@gmail.com>
2025-04-18 17:57:31 -0600

    Updates

--- dd/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [
-  "./index.html", // Asegúrate de que esté apuntando a tu archivo HTML
-  "./src/**/*.{js,ts,jsx,tsx}" // Si tienes archivos JS/TS
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
